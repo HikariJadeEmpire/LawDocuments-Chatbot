8d68a56a293d344ef8aa5db3c207387f3897bafc
HikariJadeEmpire <rastuniing@hotmail.com>
2024-02-15 18:25:08 +0700

    update

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
-import dash
+import dash, os, base64
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
+
+import upload
+from pathlib import Path
+
+main_path = Path("main.py").parent
+docs_path = Path(main_path,"..","docs_storage")
 
 app = Dash(
     title='DocsChat',
@@ -60,17 +66,79 @@ app.layout = dbc.Container([
                         "value": "CWOD",
                     },
                 ],
-                value="CWD",
+                value=None,
                 id="mode",
                 labelStyle={"display": "flex", "align-items": "center"},
             ),
             html.Br(), html.Br(),
+            html.Small(' Supported documents : ',
+                       style={'font-size':11},
+                       ), html.Br(),
+            html.Small(' .pdf, .csv, .json, .jsonl, .md, .docx, .txt, .html ',
+                       style={'font-size':10},
+                       ),
+            html.Br(), html.Br(),
             dcc.Upload(
-                dbc.Button(
-                " UPLOAD files ", outline=True, color="secondary", className="btn btn-outline-secondary",
-                style = {'font-size': 16}
-                ), style = {'textAlign': 'center',}
-            ),
+                    dbc.Button(
+                            " UPLOAD files ", outline=True, color="secondary", className="btn btn-outline-secondary",
+                            value = None,
+                            style = {'font-size': 16}
+                            ),
+                    id='docs-load',
+                    multiple=True,
+                    style = {'textAlign': 'center',}
+                ),
+            html.Br(),
+            html.Div([
+                    dbc.DropdownMenu(children=[
+                                                html.P(
+                                                    "The list of uploaded documents will appear after selecting the MODE.",
+                                                    style = {'textAlign': 'center',
+                                                            'font-size': 10}
+                                                ),
+                                            ],
+                                        label="Check documents",
+                                        # className='nav-item dropdown',
+                                        id="docs-list",
+                                        size="sm",
+                                        toggle_style={
+                                                        "background": "#909497",
+                                                    },
+                                        style = {'textAlign': 'center',},
+                                        ),
+                    ],
+                    style = {'textAlign': 'center',},
+                    ),
+            html.Br(),
+            html.Div(html.Small(children=None,
+                                id='docs-load-status'
+                                ),
+                       style={'font-size':11,
+                              'textAlign': 'center',
+                              },
+                    ),
+            html.P(id="doc-dump",
+                   style={'font-size':8,
+                        'textAlign': 'center',
+                        },
+                    ),
+            html.Br(), html.Br(), html.Br(),
+            html.Div([
+                        dbc.Button("remove all documents", outline=True, color="danger",
+                            className="btn btn-outline-danger btn-sm",
+                            id="remove-docs",
+                            n_clicks=0,
+                            style = {'textAlign': 'center'}
+                            ),
+                        html.Br(),
+                        html.Small(children=None,
+                                        id='docs-remove-status'
+                                        )
+                        ],
+                    style={'font-size':11,
+                            'textAlign': 'center',
+                            },
+                    ),
 
             ], 
             width = 2 ,
@@ -100,15 +168,78 @@ app.layout = dbc.Container([
     ])
 
 @app.callback(
-    Output("graph", "figure"), 
+        Output("docs-remove-status","children"),
+        Output('remove-docs', 'n_clicks'),
+
+        Input('remove-docs', 'n_clicks'),
+)
+def remove_docs(click):
+    status = None
+    doc_check = os.listdir(docs_path)
+    if (click >= 1) :
+        for  i in doc_check :
+            if os.path.exists(docs_path/i):
+                os.remove(docs_path/i)
+
+        doc_check = os.listdir(docs_path)
+        if (len(doc_check) < 1) :
+            click = 0
+            status = "All documents has been removed"
+            
+    elif (click == 0) :
+        status = None
+
+    return status, click
+
+@app.callback(
+        Output("doc-dump","children"),
+
+        Input('docs-load', 'contents'),
+        State('docs-load', 'filename'),
+)
+def upload_to_dir(docs, name):
+    if docs is not None :
+        for name, data in zip(name, docs):
+            data = data.encode("utf8").split(b";base64,")[1]
+            with open(os.path.join(docs_path, name), "wb") as fp:
+                fp.write(base64.decodebytes(data))
+
+@app.callback(
+    Output("docs-load-status", "children"),
+    Output("docs-list","children"),
     [
-        Input('interval', 'max_intervals'), 
-        Input('storage-df', 'data'),
-        Input("choice0", "value"),
+        Input('mode', 'value'),
+        Input('remove-docs', 'n_clicks'),
      ]
 )
-def plot_graph(max_intervals, df, coin):
-    pass
+def upload_status(mode, rm_click):
+    
+    docs_path.mkdir(parents=True, exist_ok=True)
+    doc_check = os.listdir(docs_path)
+    
+    if (len(doc_check) < 1) or (rm_click >= 1) :
+        doc_list = [dbc.DropdownMenuItem('empty',
+                                        className="text-body-tertiary",
+                                        style = {'textAlign': 'center',
+                                                'font-size': 10}
+                                        )
+                    ]
+        status = 'Please upload the documents.'
+
+    elif (len(doc_check) >= 1) and (rm_click == 0) :
+        doc_list = []
+        for i in doc_check :
+            doc_list.append(dbc.DropdownMenuItem(i,
+                                        className="text-body-tertiary",
+                                        style = {'textAlign': 'center',
+                                                'font-size': 10}
+                                        )
+                            )
+        status = 'Your documents has been uploaded'
+    
+    if mode == 'CWOD' :
+        status = None
+    return status, doc_list
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ app/upload.py
@@ -0,0 +1,158 @@
+from typing import Dict
+from langchain.document_loaders import PyPDFLoader, PyMuPDFLoader, UnstructuredMarkdownLoader, Docx2txtLoader, JSONLoader
+from langchain.document_loaders.csv_loader import CSVLoader
+
+from langchain.document_loaders import BSHTMLLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.retrievers import ParentDocumentRetriever # on testing
+
+from fixthaipdf import clean
+from pathlib import Path
+import json
+
+class LangChainDocLoaders:
+  def __init__(self, pdf_loader):
+    self.supported_doc = ['.pdf', '.csv', '.json', '.jsonl', '.md', '.docx', '.txt', '.html']
+    allowed_pdf_loaders = ["pypdf", "pymupdf"]  #Check for pdf_loader type
+    if pdf_loader not in allowed_pdf_loaders:
+        raise ValueError(f"Unsupported pdf loader type. Allowed pdf loaders are: {', '.join(allowed_pdf_loaders)}")
+    self.pdf_loader = pdf_loader
+
+  def load_document(self, document_path, *args, **kwargs) -> Dict:
+
+    #Case insensitive file extension checking
+    path_type = document_path.split('.')[-1]
+    document_path = document_path.split('.')
+    document_path[-1] = path_type.lower()
+    document_path = '.'.join(document_path)
+
+    chunk_size = kwargs.get('chunk_size', 400)
+    chunk_overlap = kwargs.get('chunk_overlap', 100)
+
+    parent_chunk_size = kwargs.get('parent_chunk_size', 2000)
+    parent_chunk_overlap = kwargs.get('parent_chunk_overlap', 0)
+
+    parent_splitter = RecursiveCharacterTextSplitter(chunk_size=parent_chunk_size, chunk_overlap=parent_chunk_overlap) # PUN Added
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap) # PUN Added
+
+    #Load PDF File.
+    if document_path.endswith(".pdf"):
+      if self.pdf_loader == "pymupdf":
+        loader = PyPDFLoader(document_path)
+        original_pages = loader.load_and_split()
+      else:
+        loader = PyMuPDFLoader(document_path)
+        original_pages = loader.load()
+
+      pages = text_splitter.split_documents(original_pages) # PUN Added
+      parent_pages = parent_splitter.split_documents(original_pages)
+
+      data = [clean(page.page_content) for page in pages]
+      parent_data = [clean(page.page_content) for page in parent_pages]
+
+      return {'child_documents':data, 'parent_documents':parent_data}
+
+    # Load CSV File.
+    elif document_path.endswith(".csv"):
+      loader = CSVLoader(file_path=document_path)
+      rows = loader.load()
+
+      data = [clean(page.page_content) for page in rows]
+
+      return {'child_documents':data, 'parent_documents':None}
+
+    #Load JSON File.
+    #For JSON and JSON line File, please provide {jq_schema} as additional argument for better extraction. example: https://python.langchain.com/docs/modules/data_connection/document_loaders/json
+    elif document_path.endswith(".json"):
+      jq_schema = kwargs.get('jq_schema', None)
+      if jq_schema:
+        loader = JSONLoader(
+          file_path=document_path,
+          jq_schema=jq_schema,
+          text_content=False
+        )
+        json_datas = loader.load()
+
+        data = [clean(page.page_content) for page in json_datas]
+
+      else:
+        print("For JSON File, please provide {jq_schema} as additional argument for better extraction. example: https://python.langchain.com/docs/modules/data_connection/document_loaders/json")
+        data = [json.loads(Path(document_path).read_text(encoding='UTF-8' ))]
+      return {'child_documents':data, 'parent_documents':None}
+
+    #Load JSON line File.
+    elif document_path.endswith(".jsonl"):
+      jq_schema = kwargs.get('jq_schema', None)
+      if jq_schema:
+        loader = JSONLoader(
+          file_path=document_path,
+          jq_schema=jq_schema,
+          text_content=False,
+          json_lines=True
+        )
+        json_datas = loader.load()
+
+        data = [clean(page.page_content) for page in json_datas]
+      else:
+        print("For JSON line File, please provide {jq_schema} as additional argument for better extraction. example: https://python.langchain.com/docs/modules/data_connection/document_loaders/json")
+        data = [Path(document_path).read_text(encoding='UTF-8' )]
+      return {'child_documents':data, 'parent_documents':None}
+
+    #Load Markdown File.
+    elif document_path.endswith(".md"):
+      loader = UnstructuredMarkdownLoader(file_path=document_path)
+      original_pages = loader.load()
+
+      pages = text_splitter.split_documents(original_pages) # PUN Added
+      parent_pages = parent_splitter.split_documents(original_pages)
+
+      data = [clean(page.page_content) for page in pages]
+      parent_data = [clean(page.page_content) for page in parent_pages]
+
+      return {'child_documents':data, 'parent_documents':parent_data}
+
+    #Load docx File.
+    elif document_path.endswith(".docx"):
+      loader = Docx2txtLoader(file_path=document_path)
+      original_pages = loader.load()
+
+      pages = text_splitter.split_documents(original_pages) # PUN Added
+      parent_pages = parent_splitter.split_documents(original_pages)
+
+      data = [clean(page.page_content) for page in pages]
+      parent_data = [clean(page.page_content) for page in parent_pages]
+
+      return {'child_documents':data, 'parent_documents':parent_data}
+
+    #Load txt File.
+    elif document_path.endswith(".txt"):
+
+      # PUN Added
+      with open(document_path, encoding="UTF-8") as f:
+        original_pages = f.read()
+
+      pages = text_splitter.create_documents([original_pages]) # PUN Added
+      parent_pages = parent_splitter.create_documents([original_pages])
+
+      data = [clean(page.page_content) for page in pages]
+      parent_data = [clean(page.page_content) for page in parent_pages]
+
+      return {'child_documents':data, 'parent_documents':parent_data}
+
+    #Load HTML File.
+    elif document_path.endswith(".html"):
+      loader = BSHTMLLoader(file_path=document_path)
+      original_pages = loader.load()
+
+      pages = text_splitter.split_documents(original_pages) # PUN Added
+      parent_pages = parent_splitter.split_documents(original_pages)
+
+      data = [clean(page.page_content) for page in pages]
+      parent_data = [clean(page.page_content) for page in parent_pages]
+
+      return {'child_documents':data, 'parent_documents':parent_data}
+
+    else:
+      raise ValueError(f"Unsupported document type. Supported documents are {self.supported_doc}")
+
+
